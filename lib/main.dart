b1e377eabd35119fc1db2e90e661ed7ebd009c51
marcturla21 <158239431+marcturla21@users.noreply.github.com>
2024-05-03 15:17:03 +0800

    Marc Turla
    adding to do list functionality

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox<String>('todos');
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Box<String> todoBox;
+  List<String> completedTasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    todoBox = Hive.box('todos');
+  }
+
+  void addToDo() {
+    TextEditingController todoController = TextEditingController();
+
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Add Todo',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: todoController,
+                decoration: InputDecoration(
+                  labelText: 'Enter Todo',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text('Cancel'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      final text = todoController.text;
+                      if (text.isNotEmpty) {
+                        final newTodo = Todo(description: text, isDone: false);
+                        todoBox.add(json.encode(newTodo.toJson()));
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: Text('Add'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
